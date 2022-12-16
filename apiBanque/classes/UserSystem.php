ff8ae3a732ce0c5512ca95dc3b265cfbd56a80a3
alexandre <alexandre.bbch@eduge.ch>
2022-12-16 10:35:18 +0100

    resolution conflit

--- apiBanque/classes/UserSystem.php
+++ apiBanque/classes/UserSystem.php
@@ -1,16 +1,7 @@
 <?php
 abstract class UserSystem
 {
-<<<<<<< HEAD
-
-public function __construct($nom,$prenom, $email, $password,$permission) {
-    $this->nom = $nom;
-    $this->prenom = $prenom;
-    $this->email = $email;
-    $this->password = $password;
-    $this->permission = $permission;
-=======
-/**
+/
  * nom
  * @var string
  */
@@ -19,42 +10,24 @@ public function __construct($name, $email, $password, $perm) {
     $this->email = $email;
     $this->password = $password;
     $this->perm = $perm
->>>>>>> 73c696307b95531c4258c4a916627e1f79bcfcbf
   }
-
-  /**
- * nom
- * @var string
- */
 protected $nom;
 
 
-/**
+/
  * prenom
  * @var string
  */
 protected $prenom;
 
-/**
+/
  * mail
  * @var string
  */
 protected $email;
-/**
-<<<<<<< HEAD
- * password
- * @var string
- */
-protected $password;
-
-
-/**
- * perm
- * @var int
-=======
+/
  * perm
  * @var int(1)
->>>>>>> 73c696307b95531c4258c4a916627e1f79bcfcbf
  */
 protected $perm;
 }
