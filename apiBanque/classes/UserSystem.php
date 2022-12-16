7bca0d8710676ecd7215d89561eec4e85eeb206e
Veloce3224 <105367409+Veloce3224@users.noreply.github.com>
2022-12-16 10:41:57 +0100

    CORRECTION DE CONFILT ENCORE

--- apiBanque/classes/UserSystem.php
+++ apiBanque/classes/UserSystem.php
@@ -1,31 +1,38 @@
 <?php
 abstract class UserSystem
 {
-/
+
+public function __construct($nom, $prenom, $email, $password, $perm) {
+    $this->nom = $nom;
+    $this->prenom = $prenom;
+    $this->email = $email;
+    $this->password = $password;
+    $this->perm = $perm;
+  }
+  /**
  * nom
  * @var string
  */
-public function __construct($name, $email, $password, $perm) {
-    $this->name = $name;
-    $this->email = $email;
-    $this->password = $password;
-    $this->perm = $perm
-  }
 protected $nom;
 
 
-/
+/**
  * prenom
  * @var string
  */
 protected $prenom;
 
-/
+/**
  * mail
  * @var string
  */
 protected $email;
-/
+/**
+ * password
+ * @var string
+ */
+protected $password;
+/**
  * perm
  * @var int(1)
  */
